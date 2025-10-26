a976249a9c5c0b4ddb5d42d9880229e8427bab6b
joseerisvaldopg <joseerisvaldopg@gmail.com>
2025-10-26 14:55:19 -0300

    feat: adding example two

---
+++ modulo 1/exemplo 2/bad/bad-boolean-names.js
@@ -0,0 +1,19 @@
+// ❌ Nomes ruins, confusos ou ambíguos
+const active = true
+const user = false
+const verified = true
+const has = false
+const done = true
+
+// ❌ Código usando essas variáveis (sem clareza)
+if (active) {
+  // O que significa "active"? Ativo em quê?
+}
+
+if (!user) {
+  // O que significa "user" ser falso? O usuário existe? Está logado? 
+}
+
+if (verified) {
+  // Verificado onde? Email, conta, identidade?
+}
---
+++ modulo 1/exemplo 2/good/good-boolean-names.js
@@ -0,0 +1,27 @@
+// ✅ Nomes claros, semânticos e autoexplicativos
+const isActive = true
+const isUserLoggedIn = false
+const isEmailVerified = true
+const hasPermission = false
+const isProcessComplete = true
+
+// ✅ Código mais legível
+if (isActive) {
+  // O usuário está ativo
+}
+
+if (!isUserLoggedIn) {
+  // O usuário não está logado
+}
+
+if (isEmailVerified) {
+  // O email foi verificado
+}
+
+if (!hasPermission) {
+  // O usuário não tem permissão
+}
+
+if (isProcessComplete) {
+  // O processo foi concluído
+}
